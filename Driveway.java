34748fc193f86b16fb6638cbcded3af08c268db5
Skumar101 <skumar1@stu.naperville203.org>
2022-09-06 11:50:53 -0500

    driveway

---
+++ Driveway.java
@@ -0,0 +1,83 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+/**
+   Class for simulating a driveway and a street, using stacks
+   of cars with license plate numbers as representation.
+*/
+public class Driveway
+{
+   /**
+      Stack representing the cars in the driveway.
+   */
+   private Stack<Integer> driveway;
+   /**
+      Stack representing the cars in the street.
+   */
+   private Stack<Integer> street;
+   /**
+      Constructor.
+   */
+   public Driveway()
+   {
+      // Complete the constructor
+      driveway = new Stack<>();
+      street = new Stack<>();
+        
+
+   }
+
+   /**
+      Add the given license plate to the driveway.
+      @param licencePlate number of license plate.
+   */
+   public void add(int licensePlate)
+   {
+      // Complete this method
+      
+        driveway.add(licensePlate);
+
+   }
+
+   /**
+      Remove the given license plate from the driveway.
+      @param licencePlate number of license plate.
+   */
+   public void remove(int licensePlate)
+   {
+      // Complete this method
+      int i = driveway.pop();
+      while(i!= licensePlate)
+      {
+          street.add(i);
+          i = driveway.pop();
+        }
+      print();
+      while(street.size()!=0)
+      {
+       driveway.add(street.pop());   
+        }
+        print();
+   }
+
+   /**
+      Prints the driveway and street details to the screen.
+   */
+   public void print()
+   {
+      System.out.println("In Driveway, starting at first in (one license plate per line):");
+      // Print the cars in the driveway here
+      for(int i : driveway)
+      {
+          System.out.println(i);
+        }
+
+      System.out.println("In Street, starting at first in (one license plate per line):");
+      // Print the cars in the street here
+      for(int j: street)
+      {
+          System.out.println(j);
+        }
+
+   }
+}
