5f74ea2d897130793250d214b1d6118897e74e8b
Skumar101 <skumar1@stu.naperville203.org>
2022-09-23 10:56:10 -0500

    grid lol

--- Grid.java
+++ Grid.java
@@ -12,41 +12,53 @@ public class Grid
    */
    public void floodfill(int row, int column)
    {
-     pixels[r][c] = count;
-     if(count<=SIZE*SIZE)
+       if(pixels[row][column] ==0)
+           {pixels[row][column]=count;}
+     if(count<(SIZE*SIZE))
      {
          count++;
+         if(row!=0)
+         {
          if(pixels[row-1][column]==0)
          {
              pixels[row-1][column] = count;
              nums.add(pixels[row-1][column]);
              count++;
             }
-            if(pixels[row][column+1]==0)
+        }
+        if(column+1!=SIZE)
+        {
+        if(pixels[row][column+1]==0)
          {
              pixels[row][column+1] = count;
              nums.add(pixels[row][column+1]);
              count++;
             }
-            if(pixels[row+1][column]==0)
+        }
+        if(row+1!=SIZE)
+        {    
+        if(pixels[row+1][column]==0)
          {
              pixels[row+1][column] = count;
              nums.add(pixels[row+1][column]);
              count++;
             }
+        }
+         if(column!=0)
+         {   
             if(pixels[row][column-1]==0)
          {
              pixels[row][column-1] = count;
              nums.add(pixels[row][column-1]);
              count++;
             }
+        }
          for(int r = 0; r< SIZE; r++)
             {
                 for(int c = 0; c<SIZE; c++)
                 {
                     if(pixels[r][c]==nums.peek())
-                        {floodfill(r, c);
-                        break;}
+                        {if(count<100){count = nums.peek();}floodfill(r, c); break;}
                 }
             }
         }
