171ea3e0f967452d1a3f265b82615dafd9a1f3c8
Skumar101 <skumar1@stu.naperville203.org>
2022-10-02 20:16:38 -0500

    forgot to push this weekend

--- TwoWordsSameHashCode.java
+++ TwoWordsSameHashCode.java
@@ -1,9 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.io.*;
+import java.util.*;
 
 /**
    Searches for words with the same hash code
@@ -18,8 +14,35 @@ public class TwoWordsSameHashCode
    */
    public static Map<Integer, HashSet<String>> generateHashMap(String filename)
    {
-       Scanner reader = new Scanner(new File(filename));
-       
+       File fl = new File(filename);
+       Scanner reader = new Scanner(System.in);
+       try {reader = new Scanner(fl);}
+       catch (IOException e){
+        }
+       Map<Integer, HashSet<String>> map = new HashMap<>();
+       ArrayList<String> values = new ArrayList<>();
+       ArrayList<Integer> keys = new ArrayList<>();
+       while(reader.hasNext())
+       {
+           String word = reader.next();
+           values.add(word);
+           keys.add(word.hashCode());
+                   }
+       //System.out.println(values);
+       //System.out.println(keys);
+       for(int i = 0; i<keys.size()-1; i++)
+       {
+           for(int j = i+1; j<keys.size(); j++)
+           {
+               HashSet<String> hset = new HashSet<>();
+               if(keys.get(i).equals(keys.get(j)))
+               {
+                   hset.add(values.get(i)); hset.add(values.get(j));
+                   map.put(keys.get(i), hset);
+                }
+            }
+        }
+        return map;
    }
 
    /**
@@ -28,7 +51,9 @@ public class TwoWordsSameHashCode
    */
    public static void printSameCodes(Map<Integer, HashSet<String>> hashCodes)
    {
-      
-
-   }
-}
+      for(Map.Entry<Integer,HashSet<String>> entry : hashCodes.entrySet())
+      {
+          System.out.println(entry.getKey()+": "+entry.getValue());
+        }
+        }
+    }
